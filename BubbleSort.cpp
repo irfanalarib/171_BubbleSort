6d8b474f74db6bed812d3439161e42c035105882
irfanalarib <irfanalarib7@gmail.com>
2025-03-13 11:09:25 +0700

    Memanggil prosedur input

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -45,6 +45,20 @@ void bubblesortArray(){
         }while(pass <= n-1); //step 5
 }
 
-int main(){
+void display(){
+    cout << endl;
+    cout << "================================" << endl;
+    cout << "Elemen Array yang telah tersusun" << endl;
+    cout << "================================" << endl;
+    cout << endl;
+    for (int j=0;j<n;j++){
+        cout << arr[j];
+        if (j<n-1) {
+            cout << " --> ";
+        }
+    }
+}
 
+int main(){
+    input ();
 }
